e2cea5d12d4d9204a23e6aa59412ec77ddd34f09
abhinav-tripathi14 <abhi.tripathi14@gmail.com>
2023-04-20 22:31:16 +0530

    stash files

---
+++ Desktop/Multiple Resource TFCLI GH Repo/main.tf
@@ -0,0 +1,15 @@
+module "createresourcetworegions" {
+  source = "./ModuleExample"    
+  
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
