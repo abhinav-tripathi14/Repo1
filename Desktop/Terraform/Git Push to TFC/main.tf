c4fce6068f28880a3a8510a4f1cbdf084273e8af
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-08-16 11:06:38 +0530

    Update main.tf

--- Desktop/Terraform/Git Push to TFC/main.tf
+++ Desktop/Terraform/Git Push to TFC/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">=1.3.6"
+   cloud {
+    organization = "Test-Abhinav"
+    workspaces {
+      name = "Repo1"
+    }
+}
+}
+
 resource "null_resource" "test"{
 count=4
 }
